3249eb7ccbef914cec3963e194c42f5754cbbc8f
vagrant <vagrant@homestead>
2023-03-10 07:07:08 +0000

    访问限流

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -12,6 +12,14 @@ use Carbon\Carbon;
 
 class PasswordController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('throttle:2,1', [
+            'only' => ['sendResetLinkEmail']
+        ]);
+    }
+
     public function showLinkRequestForm()
     {
         return view('auth.passwords.email');
